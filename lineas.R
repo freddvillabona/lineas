a89d61f7ae98c39e1cb97f68b5ff573ee11f6da7
freddvillabona <freddyvillabona9@gmail.com>
2023-01-14 23:50:34 +0100

    1
    "1"

---
+++ lineas.R
@@ -0,0 +1,64 @@
+
+
+library("ggplot2")
+library("lubridate")
+
+UK <- read.csv("Total_uk.csv", comment.char="#")
+UK$Date <- lubridate::ymd_hms(UK$Date)
+
+GLOBAL <- read.csv("Total_GLOBAL.csv", comment.char="#")
+GLOBAL$Date <- lubridate::ymd_hms(GLOBAL$Date)
+
+
+
+a <- ggplot(UK, 
+            aes(Date, n, colour=(n>20))) + 
+  geom_line(aes(group=1), size=.1) +
+  scale_colour_manual(values = c("blue", "red")) +
+  xlab("Fecha (2020)") +
+  ylab("Cantidad") +
+  theme(legend.position="none") +
+  ggtitle("<span style='font-size: 21pt;'> <b> Tuits por minuto (Reino Unido) </b></font>") +
+  theme(plot.title = element_markdown(),
+        
+        panel.background = element_rect(fill = 'white', color = 'gray'),
+        panel.grid.major = element_line(color = 'white', linetype = 'dotted'),
+        panel.grid.minor = element_line(color = 'white', size = .2),
+        
+        axis.text = element_text(size = 13),
+        axis.title = element_text(size = 14)
+        
+  )
+
+a
+
+
+b <- ggplot(Total_GLOBAL, 
+            aes(Date, n, colour=(n>750))) + 
+  geom_line(aes(group=1), size=.1) +
+  scale_colour_manual(values = c("blue", "red")) +
+  
+  
+  
+  xlab("Fecha (2020)") +
+  ylab("Cantidad") +
+  theme(legend.position="none") +
+  ggtitle("<span style='font-size: 21pt;'> <b> Tuits por minuto (Todo el mundo) </b></font>") +
+  theme(plot.title = element_markdown(),
+        
+        panel.background = element_rect(fill = 'white', color = 'gray'),
+        panel.grid.major = element_line(color = 'white', linetype = 'dotted'),
+        panel.grid.minor = element_line(color = 'white', size = .2),
+        
+        axis.text = element_text(size = 13),
+        axis.title = element_text(size = 14)
+        
+  )
+
+b
+
+
+library("patchwork")
+
+a /
+  b
