8a0b5476d7b84797d164e661c2595ba5b03e3b60
freddvillabona <freddyvillabona9@gmail.com>
2023-01-15 00:35:56 +0100

    3
    -3-

--- lineas.R
+++ lineas.R
@@ -9,16 +9,14 @@ UK$Date <- lubridate::ymd_hms(UK$Date)
 GLOBAL <- read.csv("Total_GLOBAL.csv", comment.char="#")
 GLOBAL$Date <- lubridate::ymd_hms(GLOBAL$Date)
 
-
-
 a <- ggplot(UK, 
             aes(Date, n, colour=(n>20))) + 
   geom_line(aes(group=1), size=.1) +
   scale_colour_manual(values = c("blue", "red")) +
-  xlab("Fecha (2020)") +
+  xlab("2020") +
   ylab("Cantidad") +
   theme(legend.position="none") +
-  ggtitle("<span style='font-size: 21pt;'> <b> Tuits por minuto (Reino Unido) </b></font>") +
+  ggtitle("<span style='font-size: 21pt;'> <b>COVID-19: tuits por minuto (Reino Unido) </b></font>") +
   theme(plot.title = element_markdown(),
         
         panel.background = element_rect(fill = 'white', color = 'gray'),
@@ -30,20 +28,14 @@ a <- ggplot(UK,
         
   )
 
-a
-
-
-b <- ggplot(Total_GLOBAL, 
+b <- ggplot(GLOBAL, 
             aes(Date, n, colour=(n>750))) + 
   geom_line(aes(group=1), size=.1) +
   scale_colour_manual(values = c("blue", "red")) +
-  
-  
-  
-  xlab("Fecha (2020)") +
+  xlab("2020") +
   ylab("Cantidad") +
   theme(legend.position="none") +
-  ggtitle("<span style='font-size: 21pt;'> <b> Tuits por minuto (Todo el mundo) </b></font>") +
+  ggtitle("<span style='font-size: 21pt;'> <b>COVID-19: tuits por minuto (Todo el mundo) </b></font>") +
   theme(plot.title = element_markdown(),
         
         panel.background = element_rect(fill = 'white', color = 'gray'),
@@ -54,8 +46,6 @@ b <- ggplot(Total_GLOBAL,
         axis.title = element_text(size = 14)
         
   )
-
-b
 
 
 library("patchwork")
